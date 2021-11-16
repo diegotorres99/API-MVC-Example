1419c418ed8e55c2e95216dd1a8daecc9008cc13
Luis Diego Torres <luis.torres.alvarez@cuc.cr>
2021-11-16 17:53:27 -0600

    V 1.0

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        // Default Method 
+
+        public IActionResult Index()
+        {
+            //Especifica uso de plantilla de vista para presentar info
+            //Plantilla predetermina index
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //El ViewDataobjeto de 
+            //    diccionario contiene datos que se pasarán a la vista
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HtmlEncoder.Default
+        //        .Encode($"Hello {name}, ID: {ID}");
+        //}
+    }
+}
